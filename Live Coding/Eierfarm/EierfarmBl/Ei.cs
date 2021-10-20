3c206d3cf0de8301d40b54360cd78115701560b7
Olaf Lischke <olaf.lischke@lischke-edv.de>
2021-10-20 16:26:01 +0200

    Seminar Tag 1

---
+++ Live Coding/Eierfarm/EierfarmBl/Ei.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace EierfarmBl
+{
+    public class Ei
+    {
+        // Irgendwo anders:
+        // Ei ei = new Ei();
+        public Ei()
+        {
+            this.Legedatum = DateTime.Now;
+
+            Random random = new Random();
+            this.Gewicht = random.Next(45, 81);
+            this.Farbe = (EiFarbe)random.Next(Enum.GetNames(typeof(EiFarbe)).Length);
+            // DirectCast - liefert Exception wenn Cast fehlschlägt
+        }
+        // Ei ei=new Ei(50);
+        public Ei(int gewicht) : this()
+        {
+            this.Gewicht = gewicht;
+        }
+        // Backing Field für die Gewicht-Property
+        private double _gewicht;
+
+        // Full-qualified Property (propfull)
+        // Property mit Backing Field
+        public double Gewicht
+        {
+            get { return _gewicht; }  // double g = meinEi.Gewicht
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception();
+                }
+                _gewicht = value;
+            } // meinEi.Gewicht = value
+        }
+
+        // Auto-Property
+        // Property mit automatisch (durch den Compiler)
+        // generiertem Backing Field
+        public int Id { get; private set; }
+
+        public DateTime Legedatum { get; set; }
+        public EiFarbe Farbe { get; set; }
+    }
+
+    public enum EiFarbe
+    {
+        Hell,
+        Dunkel,
+        Gruen
+    }
+}
---
+++ Live Coding/Eierfarm/EierfarmBl/Huhn.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EierfarmBl
+{
+    public class Huhn
+    {
+        public Huhn(string name)
+        {
+            this.Name = name;
+            this.Gewicht = 1000;
+            this.Id = Guid.NewGuid();
+        }
+
+        public void Fressen()
+        {
+            if (this.Gewicht <= 3000)
+            {
+                // this.Gewicht = this.Gewicht + 100;
+                this.Gewicht += 100;
+            }
+        }
+
+        public void EiLegen()
+        {
+            if (this.Gewicht>1500 && !this.Eier.Any(ei => DateTime.Now < ei.Legedatum.AddHours(3)))
+            {
+                Ei ei = new Ei();
+                this.Gewicht -= ei.Gewicht;
+                this.Eier.Add(ei);
+            }
+
+        }
+
+        //public DateTime LetztesEiLegen { get; set; }
+        public double Gewicht { get; set; }
+        public string Name { get; set; }
+        public Guid Id { get; set; }
+
+        public List<Ei> Eier { get; set; }
+    }
+}
+
